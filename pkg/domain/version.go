ffdb6980555c402e7e2de742da3798487ce1fed3
TurnsCoffeeIntoScripts <turns.coffee.into.scripts@gmail.com>
2019-05-21 13:43:05 -0400

    Added comments to version.go in the domain package

--- pkg/domain/version.go
+++ pkg/domain/version.go
@@ -1,5 +1,9 @@
+/*
+Package 'domain' provides struct that corresponds to Jira REST API objects
+ */
 package domain
 
+// Simplified representation of the 'version' object from the Jira REST API.
 type Version struct {
 	Name string `json:"name"`
 }
