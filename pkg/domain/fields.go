2a7df7a224ca3efe2786bcad42611fd90b99fbd8
TurnsCoffeeIntoScripts <turns.coffee.into.scripts@gmail.com>
2019-05-21 13:34:51 -0400

    Code formatting and spelling mistakes

--- pkg/domain/fields.go
+++ pkg/domain/fields.go
@@ -1,7 +1,7 @@
 package domain
 
 type Fields struct {
-	Versions []Version        `json:"verions"`
+	Versions []Version        `json:"versions"`
 	Status   Status           `json:"status"`
 	Comment  CommentContainer `json:"comment"`
 }
--- pkg/domain/issue.go
+++ pkg/domain/issue.go
@@ -1,7 +1,7 @@
 package domain
 
 type Issue struct {
-	Key string `json:"key"`
-	Id string `json:"id"`
-	Fields Fields `json:"fields"`
+    Key    string `json:"key"`
+    Id     string `json:"id"`
+    Fields Fields `json:"fields"`
 }
