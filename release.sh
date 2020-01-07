71e7e5309970114f2f859532565dc2a8a357f337
Guillaume Rivest <guillaume.rivest@desjardins.com>
2020-01-07 16:09:33 -0500

    Added release script to make it easier to create new Docker image with git tag

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Utility script to simplify release procedure.
+# Need to have credentials to both github and dockerhub to use.
+
+# Building docker image
+docker image build -t turnscoffeeintoscripts/jira-api-resource:$1 .
+
+if [[ $? -eq "0" ]]; then
+    # Pushing docker image
+    docker image push turnscoffeeintoscripts/jira-api-resource:$1
+else
+    exit 1
+fi
+
+if [[ $? -eq "0" ]]; then
+    # Tag in git when the image was successfully push
+    git tag -a $1 -m "Tagging version $1"
+else
+    exit 1
+fi
